965fb48a025d28a0696e5012634ceb74ae382802
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2023-04-12 19:06:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function handleClick() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:8080/');
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        console.log(xhr.response);
+      }else{
+        console.log(xhr.response);
+      }
+    };
+    xhr.send();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +32,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <button onClick={handleClick}>Get Data</button>
     </div>
   );
 }
